5a459b8dce7368e40a05c646f5a1131b2f34c178
natboon <natthida.bo@kkumail.com>
2024-05-11 21:33:29 +0700

    add create2

---
+++ pages/create/createorder.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>createorder</h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    layout: 'default'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/create/index.vue
+++ pages/create/index.vue
@@ -6,29 +6,50 @@
             <i class="pi pi-search absolute top-2/4 -mt-1 right-5 text-surface-400 dark:text-surface-600"/>
         </span>
         <span class="buttoncreate">
-            <Button label="สร้างออเดอร์" icon="pi pi-plus-circle" class="w-[200px] h-[40px] text-lg bg-[green] mr-2 mt-3"/>
+            <Button label="สร้างออเดอร์" icon="pi pi-plus-circle" class="w-[200px] h-[40px] text-lg bg-[green] mr-2 mt-3"  @click="goToCreateOrder"/>
         </span>
     </div>
     <div class="flex">
-    <div class="flex justify-between gap-2 w-[1200px] h-[300px] text-[16px] font-semibold rounded-lg bg-[white] p-4 relative">
-        <div>
-            <span class="block mb-3">สถานะออเดอร์</span>
+    <div class="flex flex-col gap-4 w-[1200px] h-[300px] text-[16px] font-semibold rounded-lg bg-[white] p-4 relative">
+        <div class="flex justify-between">
+            <span class="block mb-3">ออเดอร์</span>
+            <input type="datetime-local" id="meeting-time"
+                name="meeting-time" value="2024-05-10T19:30"
+                class="rounded-lg border border-gray-300 shadow-sm h-[40px]">
             <span class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
-                <svg class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round">
-                    </path>
-                </svg>
+            <svg class="h-6 w-6 text-gray-400" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round">
+                </path>
+            </svg>
             </span>
         </div>
-        <input type="datetime-local" id="meeting-time"
-               name="meeting-time" value="2024-05-10T19:30"
-               class="rounded-lg border border-gray-300 shadow-sm h-[40px]">
+
+        <div>
+            <div class="flex bg-[#84A390] text-[white] rounded-lg p-2 ">
+                <span class="block mb-3">เลขออเดอร์</span>
+                <span class="block mb-3">วันที่สั่งซื้อ</span>
+                <span class="block mb-3">สถานะออเดอร์</span>
+                <span class="block mb-3">ชื่อลูกค้า</span>
+                <span class="block mb-3">ช่องทางการสั่งซื้อ</span>
+                <span class="block mb-3">การจัดส่ง</span>
+                <span class="block mb-3">ยอดขาย</span>
+                <span class="block mb-3">หมายเหตุ</span>
+            </div>
+        </div>
     </div>
     </div>
 
 </template>
 
 <script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const goToCreateOrder = () => {
+    router.push('/create/createorder');
+};
+
 definePageMeta({
     layout: 'default'
 })
