85418c1ac9efbe72ffc3eee26378aa62332358e9
Nat <nattaphon_wo@kkumail.com>
2024-05-18 10:49:00 +0700

    update navigation asidebar and fix sale menu add interactive action with user when user click button add the modal will slide from right hand into screen

--- components/sidebar.vue
+++ components/sidebar.vue
@@ -1,13 +1,26 @@
+<script setup>
+import { navigationMenu } from "./component/sidebarObject";
+import { useRoute } from "vue-router";
+const route = useRoute();
+
+const currentPath = ref(route.path);
+
+watch(() => route.path, (newPath) => {
+  currentPath.value = newPath;
+});
+</script>
+
 <template>
   <div class="flex h-screen">
     <!-- Sidebar -->
     <div class="bg-[#FFFFFF] text-black w-[300px] flex-shrink-0">
       <ul class="w-full flex flex-col">
-        <li class="flex justify-center w-full items-center text-[24px]" v-for="item in navigationMenu" :key="item.id">
-          <NuxtLink
-            :to="item.path"
-            class="gap-2 h-[100px] px-10 w-full rounded hover:bg-[#CDDAD2] flex items-center justify-center"
-          >
+        <li :class="[
+          'flex justify-center w-full items-center text-[24px]',
+          { 'bg-slate-500': currentPath == item.path }
+        ]" v-for="item in navigationMenu" :key="item.id">
+          <NuxtLink :to="item.path"
+            class="ease duration-75 gap-2 h-[100px] px-10 w-full rounded hover:bg-[#CDDAD2] flex items-center justify-center">
             <i :class="item.icon" style="font-size: 30px"></i>
             <p class="w-full">
               {{ item.page }}
@@ -19,8 +32,6 @@
   </div>
 </template>
 
-<script setup>
-import { navigationMenu } from "./component/sidebarObject";
-</script>
+
 
 <style lang="scss" scoped></style>
---
+++ pages/sale/partial/salecreate.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="flex flex-col gap-4 w-auto min-w-[200px] max-w-[550px] h-[600px] text-[16px] font-semibold rounded-lg bg-[white] p-4 relative mr-2 shadow-[3px_4px_4px_rgba(0,0,0,0.25)]">
+    <div class="card flex justify-content-center">
+      <span
+        class=" max-w-full w-full text-center py-2 rounded-full border border-[#000000] text-[18px]">เพิ่มสินค้า</span>
+      <!-- <Textarea v-model="text" rows="1" cols="20" disabled style="margin: auto;" class="centered-textarea" /> -->
+    </div>
+    <div class="flex justify-between gap-2">
+      <div class="flex-col gap-2">
+        <div class="flex flex-col gap-1 mt-1">
+          <label for="username">รหัสสินค้า</label>
+          <InputText id="username" v-model="value" class="w-[250px] h-[40px]" />
+        </div>
+        <div class="flex flex-col gap-1 mt-1">
+          <label for="address">ชื่อสินค้า</label>
+          <InputText id="address" v-model="value" class="w-[250px] h-[40px]" />
+        </div>
+        <div class="flex flex-col gap-1 mt-1">
+          <label for="zipcode">ราคา</label>
+          <InputText id="zipcode" v-model="value" class="w-[250px] h-[40px]" />
+        </div>
+        <div class="flex flex-col gap-1 mt-1">
+          <label for="phone">หน่วย</label>
+          <InputText id="phone" v-model="value" class="w-[250px] h-[40px]" />
+        </div>
+        <div class="flex flex-col gap-1 mt-1">
+          <label for="phone">จำนวน</label>
+          <InputText id="phone" v-model="value" class="w-[250px] h-[40px]" />
+        </div>
+        <div class="flex flex-col gap-1 mt-1">
+          <label for="phone">หมวดหมู่</label>
+          <InputText id="phone" v-model="value" class="w-[250px] h-[40px]" />
+        </div>
+      </div>
+      <div class="bg-gray-300 w-[300px] h-full rounded-md"></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, defineProps } from 'vue';
+defineProps({
+  open: Boolean,
+})
+const text = ref('เพิ่มรายการสินค้า');
+</script>
