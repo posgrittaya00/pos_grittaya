0adae8c5fb25bc5423e7aa638427950c791b03ca
Makeyabe <96216638+Makeyabe@users.noreply.github.com>
2024-05-21 19:28:20 +0700

    button

--- pages/sale/partial/salecreate.vue
+++ pages/sale/partial/salecreate.vue
@@ -1,36 +1,34 @@
 <template>
   <div
-    class="flex flex-col gap-4 w-auto min-w-[200px] max-w-[550px] h-[600px] text-[16px] font-semibold rounded-lg bg-[white] p-4 relative mr-2 shadow-[3px_4px_4px_rgba(0,0,0,0.25)]">
-    <div class="card flex justify-content-center">
-      <span
-        class=" max-w-full w-full text-center py-2 rounded-full border border-[#000000] text-[18px]">เพิ่มสินค้า</span>
-      <!-- <Textarea v-model="text" rows="1" cols="20" disabled style="margin: auto;" class="centered-textarea" /> -->
+    class="flex flex-col gap-4 w-auto min-w-[200px] max-w-[550px] h-[700px] text-[16px] font-semibold rounded-lg bg-white p-4 relative mr-2 shadow-[3px_4px_4px_rgba(0,0,0,0.25)]">
+    <div class="card flex justify-center">
+      <span class="max-w-full w-full text-center py-2 rounded-full border border-black text-[18px]">เพิ่มสินค้า</span>
     </div>
-    <div class="flex justify-between gap-2">
-      <div class="flex-col gap-2">
+    <div class="flex justify-between gap-2 flex-grow">
+      <div class="flex flex-col gap-2">
         <div class="flex flex-col gap-1 mt-1">
-          <label for="username">รหัสสินค้า</label>
-          <InputText id="username" v-model="value" class="w-[250px] h-[40px]" />
+          <label for="product-code">รหัสสินค้า</label>
+          <InputText id="product-code" v-model="productCode" class="w-[250px] h-[40px]" />
         </div>
         <div class="flex flex-col gap-1 mt-1">
-          <label for="address">ชื่อสินค้า</label>
-          <InputText id="address" v-model="value" class="w-[250px] h-[40px]" />
+          <label for="product-name">ชื่อสินค้า</label>
+          <InputText id="product-name" v-model="productName" class="w-[250px] h-[40px]" />
         </div>
         <div class="flex flex-col gap-1 mt-1">
-          <label for="zipcode">ราคา</label>
-          <InputText id="zipcode" v-model="value" class="w-[250px] h-[40px]" />
+          <label for="price">ราคา</label>
+          <InputText id="price" v-model="price" class="w-[250px] h-[40px]" />
         </div>
         <div class="flex flex-col gap-1 mt-1">
-          <label for="phone">หน่วย</label>
-          <InputText id="phone" v-model="value" class="w-[250px] h-[40px]" />
+          <label for="unit">หน่วย</label>
+          <InputText id="unit" v-model="unit" class="w-[250px] h-[40px]" />
         </div>
         <div class="flex flex-col gap-1 mt-1">
-          <label for="phone">จำนวน</label>
-          <InputText id="phone" v-model="value" class="w-[250px] h-[40px]" />
+          <label for="quantity">จำนวน</label>
+          <InputText id="quantity" v-model="quantity" class="w-[250px] h-[40px]" />
         </div>
         <div class="flex flex-col gap-1 mt-1">
-          <label for="phone">หมวดหมู่</label>
-          <InputText id="phone" v-model="value" class="w-[250px] h-[40px]" />
+          <label for="category">หมวดหมู่</label>
+          <InputText id="category" v-model="category" class="w-[250px] h-[40px]" />
         </div>
       </div>
       <div class="container">
@@ -39,10 +37,14 @@
           <div>เพิ่มรูปภาพสินค้า</div>
         </div>
       </div>
-      
+    </div>
+    <div class="flex gap-3 mt-1">
+      <Button label="ยกเลิก" severity="secondary" outlined class="flex-grow w-[80px]" @click="_" />
+      <Button label="สร้างออเดอร์" class="flex-grow w-[80px]"  @click="_" />
     </div>
   </div>
 </template>
+
 
 <script setup>
 import { ref, defineProps } from 'vue';
