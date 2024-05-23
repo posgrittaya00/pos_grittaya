3d6cbba7935fe5cc3cb2780b768fbf94e75f2f17
rattapong panjaroen <96216514+rattapong1711@users.noreply.github.com>
2024-05-23 15:54:14 +0700

    new code add stock

--- pages/stock/index.vue
+++ pages/stock/index.vue
@@ -1,25 +1,92 @@
+<script setup>
+import { useRouter } from 'vue-router';
+import salecreate from './new/stockcreate.vue';
+const router = useRouter();
+const OpenSaleCreate = ref(true)
+const goTosalecreate = () => {
+  OpenSaleCreate.value = !OpenSaleCreate.value;
+};
+
+definePageMeta({
+  layout: 'default'
+})
+</script>
+
+
 <template>
-    <div class="flex">
-      <div  class="flex justify-between">
-        <span class="mt-1">
-          <Dropdown v-model="selectedCity" editable :options="cities" optionLabel="name" placeholder="สถานะ" class="w-full md:w-[8rem]" />
-        </span>
-        <span class="mt-1">
-          <Dropdown v-model="selectedCity" editable :options="cities" optionLabel="name" placeholder="หมวดหมู่" class="w-full md:w-[9rem]" />
-        </span>
-        <span class="relative">
-          <InputText v-model="value1" placeholder="ค้นหาออเดอร์" class="w-[300px] h-[40px] mr-2 mt-3 pl-10 placeholder-shift" />
-          <i class="pi pi-search absolute top-1/2 transform -translate-y-1/2 left-3 text-surface-400 dark:text-surface-600"/>
-        </span>
-        <span class="buttoncreate">
-          <Button icon="pi pi-plus" class="w-[100px] h-[40px] text-lg bg-[#326035] mr-2 mt-3" @click="goTosalecreate"/>
-        </span>
+  <div class="flex gap-2 mt-2">
+    <div
+      class="flex flex-col gap-4 w-[1200px] h-[300px] text-[16px] font-semibold rounded-lg rounded-tr-lg bg-[white] relative ">
+      <div class="flex shadow-[0px_4px_4px_rgb(0,0,0,0.25)] py-4 rounded-b-md">
+        <div class="px-3">
+          <div class="flex justify-between gap-5 items-center">
+            <span>
+              <Dropdown v-model="selectedCity" editable :options="cities" optionLabel="name" placeholder="สถานะ"
+                class="w-full md:w-[8rem] h-[40px]" />
+            </span>
+            <span>
+              <Dropdown v-model="selectedCity" editable :options="cities" optionLabel="name" placeholder="หมวดหมู่"
+                class="w-full md:w-[9rem] h-[40px]" />
+            </span>
+            <span class="relative">
+              <InputText v-model="value1" placeholder="ค้นหาออเดอร์"
+                class="w-[700px] h-[40px] mr-2 pl-10 placeholder-shift" :pt="{
+                  root: ({ props, context, parent }) => {
+                    var _a;
+                    return {
+                      class: [
+                        // Font
+                        'leading-[normal]',
+                        // Flex
+                        { 'flex-1 w-[1%]': parent.instance.$name == 'InputGroup' },
+                        // Spacing
+                        'm-0',
+                        {
+                          'px-4 py-4': props.size == 'large',
+                          'px-2 py-2': props.size == 'small',
+                          'p-3': props.size == null
+                        },
+                        // Shape
+                        { 'rounded-md': parent.instance.$name !== 'InputGroup' },
+                        { 'first:rounded-l-md rounded-none last:rounded-r-md': parent.instance.$name == 'InputGroup' },
+                        { 'border-0 border-y border-l last:border-r': parent.instance.$name == 'InputGroup' },
+                        { 'first:ml-0 -ml-px': parent.instance.$name == 'InputGroup' && !props.showButtons },
+                        // Colors
+                        'text-surface-800 dark:text-white/80',
+                        'placeholder:text-white dark:placeholder:text-white',
+                        'bg-surface-0 dark:bg-surface-900',
+                        'border',
+                        { 'border-surface-300 dark:border-surface-600': !props.invalid },
+                        // Invalid State
+                        'invalid:focus:ring-red-200',
+                        'invalid:hover:border-red-500',
+                        { 'border-red-500 dark:border-red-400': props.invalid },
+                        // States
+                        {
+                          'hover:border-primary': !context.disabled && !props.invalid,
+                          'focus:outline-none focus:outline-offset-0 focus:ring focus:ring-primary-500/50 dark:focus:ring-primary-400/50 focus:z-10': !context.disabled,
+                          'opacity-60 select-none pointer-events-none cursor-default': context.disabled
+                        },
+                        // Filled State *for FloatLabel
+                        { filled: ((_a = parent.instance) == null ? void 0 : _a.$name) == 'FloatLabel' && context.filled },
+                        // Misc
+                        'appearance-none',
+                        'transition-colors duration-200'
+                      ]
+                    };
+                  }
+                }" />
+              <i
+                class="pi pi-search absolute top-1/2 transform -translate-y-1/2 left-3 -mt-1 text-surface-400 dark:text-surface-600" />
+            </span>
+            <span class="buttoncreate">
+              <Button icon="pi pi-plus" class="w-[100px] h-[40px] text-lg bg-[#326035] mr-2" @click="goTosalecreate" />
+            </span>
+          </div>
+        </div>
       </div>
-    </div>
-    <div class="flex flex-col gap-4 w-[1200px] h-[300px] text-[16px] font-semibold rounded-b-lg rounded-tr-lg bg-[white] p-4 relative">
-      <div class="flex w-full">
-        <div class="flex w-full justify-evenly bg-[#FFFFFF] text-[#000000] rounded-lg p-2">
-          <DataTable :value="products" tableStyle="min-width: 72rem">
+        <div class="flex flex-col gap-2 w-full h-[500px] text-[16px] font-semibold rounded-b-lg bg-[white] p-3 relative mr-2">
+          <DataTable :value="products" tableStyle="min-width: 50rem">
             <Column field="code" header="รหัสสินค้า"></Column>
             <Column field="name" header="ชื่อสินค้า"></Column>
             <Column field="price" header="ราคา"></Column>
@@ -29,38 +96,39 @@
             <Column field="status" header="สถานะ"></Column>
           </DataTable>
         </div>
-      </div>
     </div>
+    <div :class="[
+      'ease-in-out',
+      'duration-200',
+      { 'w-0 h-0 translate-x-[999px]': OpenSaleCreate },
+    ]">
+      <salecreate :open="OpenSaleCreate" /> 
+    </div>
+  </div>
 </template>
 
-<script setup>
-import { useRouter } from 'vue-router';
-
-const router = useRouter();
-const goTosalecreate = () => {
-router.push('/sale/salecreate');
-};
-
-definePageMeta({
-layout: 'default'
-})
-</script>
 
 <style>
 .placeholder-shift::placeholder {
-color: #999; /* ตัวอย่างสีของ placeholder */
-font-size: 14px; /* ตัวอย่างขนาดของ placeholder */
-transform: translateY(-2px); /* ขยับ placeholder ลงหรือขึ้นตามต้องการ */
+  color: #999;
+  /* ตัวอย่างสีของ placeholder */
+  font-size: 14px;
+  /* ตัวอย่างขนาดของ placeholder */
+  transform: translateY(-2px);
+  /* ขยับ placeholder ลงหรือขึ้นตามต้องการ */
 }
 
 .relative .pi-search {
-transform: translateY(-50%);
+  transform: translateY(-50%);
 }
 
 .relative .pi-search {
-top: 60%; /* ปรับค่าตรงนี้เพื่อขยับไอคอนขึ้นหรือลง */
-left: 10px; /* ปรับค่าตรงนี้เพื่อขยับไอคอนไปทางซ้ายหรือขวา */
-transform: translateY(-50%);
-color: #999; /* ตัวอย่างสีของไอคอน */
+  top: 60%;
+  /* ปรับค่าตรงนี้เพื่อขยับไอคอนขึ้นหรือลง */
+  left: 10px;
+  /* ปรับค่าตรงนี้เพื่อขยับไอคอนไปทางซ้ายหรือขวา */
+  transform: translateY(-50%);
+  color: #999;
+  /* ตัวอย่างสีของไอคอน */
 }
 </style>
---
+++ pages/stock/new/stockcreate.vue
@@ -0,0 +1,106 @@
+<template>
+    <div
+        class="flex-col gap-4 w-auto min-w-[200px] max-w-[550px] h-[700px] text-[16px] font-semibold rounded-lg bg-white p-4 relative mr-2 shadow-[3px_4px_4px_rgba(0,0,0,0.25)]">
+        <div class="card flex justify-center">
+            <span class="max-w-full w-full text-center py-2 rounded-full border border-clack text-[28px]">เพิ่มสินค้า</span>
+        </div>
+        <div class="flex justify-between gap-2 flex-grow">
+            <div class="flex flex-col gap-2">
+                <div class="flex flex-col gap-1 mt-1">
+                    <label for="product-code">รหัสสินค้า</label>
+                    <InputText id="product-code" v-model="productCode" class="w-[250px] h-[40px]" />
+                </div>
+                <div class="flex flex-col gap-1 mt-1">
+                    <label for="product-name">ชื่อสินค้า</label>
+                    <InputText id="product-name" v-model="productName" class="w-[250px] h-[40px]" />
+                </div>
+                <div class="flex flex-col gap-1 mt-1">
+                    <label for="price">ราคา</label>
+                    <InputText id="price" v-model="price" class="w-[250px] h-[40px]" />
+                </div>
+                <div class="flex flex-col gap-1 mt-1">
+                    <label for="unit">หน่วย</label>
+                    <InputText id="unit" v-model="unit" class="w-[250px] h-[40px]" />
+                </div>
+                <div class="flex flex-col gap-1 mt-1">
+                    <label for="quantity">จำนวน</label>
+                    <InputText id="quantity" v-model="quantity" class="w-[250px] h-[40px]" />
+                </div>
+                <div class="flex flex-col gap-1 mt-1">
+                    <label for="category">หมวดหมู่</label>
+                    <InputText id="category" v-model="category" class="w-[250px] h-[40px]" />
+                </div>
+            </div>
+            <div class="container">
+                <div class="bg-gray-300 w-[250px] h-full rounded-md flex justify-center items-center">
+                    <span class="pi pi-plus-circle" style="font-size: 2rem;"></span>
+                    <div>เพิ่มรูปภาพสินค้า</div>
+                </div>
+            </div>
+        </div>
+        <div class="flex gap-3 mt-1">
+            <Button label="ยกเลิก" severity="danger" class="flex-grow w-[80px]" @click="_"/>
+            <Button label="บันทึก" class="flex-grow w-[80px]" @click="_"/>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import { ref, defineProps } from 'vue';
+defineProps({
+  open: Boolean,
+})
+const text = ref('เพิ่มรายการสินค้า');
+import Swal from 'sweetalert2'
+export default {
+  data () {
+    return {
+      productCode: '',
+      productName: '',
+      price: '',
+      unit: '',
+      quantity: '',
+      category: '',
+      
+    }
+  },
+  methods: {
+    async createProduct () {
+      await this.$axios
+        .post('http://localhost:8000/api/auth/register', {
+          productCode: this.productCode,
+          productName: this.productName,
+          price: this.price,
+          unit: this.unit,
+          quantity: this.quantity,
+          category: this.category,
+
+        })
+        .then(function (response) {
+          if (response.status === 201) {
+            Swal.fire({
+              icon: 'success',
+              title: 'Success',
+              text: 'You have successfully added product ',
+              confirmButtonText: 'OK'
+            }).then((result) => {
+              if (result.value) {
+                window.location.href = 'http://localhost:3000/sale'
+              }
+            })
+          }
+        })
+        .catch(function (error) {
+          if (error.response.status === 400) {
+            Swal.fire({
+              icon: 'error',
+              title: 'Oops...',
+              text: 'Please correct your information'
+            })
+          }
+        })
+    }
+  }
+}
+</script>
