e352f844197c852752c79e01bf07c1054782ef63
rattapong panjaroen <96216514+rattapong1711@users.noreply.github.com>
2024-05-29 11:42:11 +0700

    sale

--- pages/edit/index.vue
+++ pages/edit/index.vue
@@ -12,28 +12,82 @@
         </div>
     </div>
     <!-- กล่องที่ 2 -->
-    <div class="flex">
-        <div class="flex flex-col gap-4 w-full h-[500px] text-[16px] font-semibold rounded-b-lg bg-[white] p-4 relative mr-2">
-            <DataTable :value="products" tableStyle="min-width: 50rem">
-                <Column field="codeproduct" header="รหัสสินค้า"></Column>
-                <Column field="nameproduct" header="ชื่อสินค้า"></Column>
-                <Column field="price" header="ราคา(บาท)"></Column>
-                <Column field="quantity" header="จำนวน"></Column>
-                <Column field="unit" header="หน่วย"></Column>
-                <Column field="category" header="หมวดหมู่"></Column>
-            </DataTable>
+    <div class="flex w-full">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-b-lg w-full">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            รหัสสินค้า
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ชื่อสินค้า
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            ราคา
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            จำนวน
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            หน่วย
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            หมวดหมู่
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            12345
+                        </th>
+                        <td class="px-6 py-4">
+                            มาม่า
+                        </td>
+                        <td class="px-6 py-4">
+                            7
+                        </td>
+                        <td class="px-6 py-4">
+                            10
+                        </td>
+                        <td class="px-6 py-4">
+                            ซอง
+                        </td>
+                        <td class="px-6 py-4">
+                            อาหารแห้ง
+                        </td>
+                        <td class="px-6 py-4">
+                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">แก้ไขสต็อก</a>
+                        </td>
+                        <td class="flex items-center px-6 py-4">
+                            <a href="#" class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3">ลบสินค้า</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </template>
 
 <script setup>
-const selectedCategory = ref();
+import { ref } from 'vue';
+
+const selectedCategory = ref(null);
 const category = ref([
-    { name: 'คลังสินค้าที่ขาย', code: 'SA' }, //อย่าลืมแก้
+    { name: 'คลังสินค้าที่ขาย', code: 'SA' },
     { name: 'คลังสินค้าที่สต็อก', code: 'ST' }
 ]);
 </script>
 
 <style lang="scss" scoped>
-
+.flex {
+    width: 100%;
+}
 </style>
